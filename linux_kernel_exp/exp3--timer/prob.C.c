9e329f1d8510c99277cc03ea8dfb9a93954c4971
XuChongyang <giantpoplar@163.com>
2016-05-09 07:36:29 -0600

    Update prob.C.c

--- linux_kernel_exp/exp3--timer/prob.C.c
+++ linux_kernel_exp/exp3--timer/prob.C.c
@@ -9,8 +9,8 @@ static void psig_virtual(void);
 static void psig_prof(void);
 
 static void c1sig_real(void);
-static void c1ig_virtual(void);
-static void c1ig_prof(void);
+static void c1sig_virtual(void);
+static void c1sig_prof(void);
 
 static void c2sig_real(void);
 static void c2sig_virtual(void);
@@ -34,6 +34,7 @@ int main(int argc, char**argv){
 	int i;
 	if(argc < 4){
 		printf("Usage: prob.C arg1 arg2 arg3\n");
+		return;
 	}
 	
 	//parent progress
@@ -90,11 +91,11 @@ int main(int argc, char**argv){
 		
 		// 打印子进程 1所花费的 3种时间值
 		getitimer(ITIMER_REAL,&c1_realt);
-		printf("Child1 fib = %d \n Child1 RealTime=%ld sec : %ld msec\n",fib,cl_real_secs + 9 -cl_realt.it_value.tv_sec,999999-cl_realt.it_value.tv_usec/1000);
+		printf("Child1 fib = %d \nChild1 RealTime=%ld sec : %ld msec\n",fib,c1_real_secs + 9 -c1_realt.it_value.tv_sec,999999-c1_realt.it_value.tv_usec/1000);
 		getitimer(ITIMER_VIRTUAL,&c1_virtt);
-		printf("Child1 fib = %d \n Child1 VirtualTime=%ld sec : %ld msec\n",fib,c1_virtual_secs + 9 -cl_virtt.it_value.tv_sec,999999-cl_virtt.it_value.tv_usec/1000);
+		printf("Child1 VirtualTime=%ld sec : %ld msec\n",fib,c1_virtual_secs + 9 -c1_virtt.it_value.tv_sec,999999-c1_virtt.it_value.tv_usec/1000);
 		getitimer(ITIMER_PROF,&c1_virtt);
-		printf("Child1 fib = %d \n Child1 ProfTime=%ld sec : %ld msec\n",fib,c1_prof_secs + 9 -cl_proft.it_value.tv_sec,999999-cl_proft.it_value.tv_usec/1000);
+		printf("Child1 ProfTime=%ld sec : %ld msec\n",fib,c1_prof_secs + 9 -c1_proft.it_value.tv_sec,999999-c1_proft.it_value.tv_usec/1000);
 		
 	}
 	else {
@@ -126,25 +127,24 @@ int main(int argc, char**argv){
 			
 			fib = fibonacci(atoi(argv[2]));
 			
-			// 打印子进程 1所花费的 3种时间值
+			// 打印子进程 2所花费的 3种时间值
 			getitimer(ITIMER_REAL,&c2_realt);
-			printf("Child2 fib = %d \n Child2 RealTime=%ld sec : %ld msec\n",fib,c2_real_secs + 9 -c2_realt.it_value.tv_sec,999999-c2_realt.it_value.tv_usec/1000);
+			printf("Child2 fib = %d \nChild2 RealTime=%ld sec : %ld msec\n",fib,c2_real_secs + 9 -c2_realt.it_value.tv_sec,999999-c2_realt.it_value.tv_usec/1000);
 			getitimer(ITIMER_VIRTUAL,&c2_virtt);
-			printf("Child2 fib = %d \n Child2 VirtualTime=%ld sec : %ld msec\n",fib,c2_virtual_secs + 9 -c2_virtt.it_value.tv_sec,999999-c2_virtt.it_value.tv_usec/1000);
+			printf("Child2 VirtualTime=%ld sec : %ld msec\n",fib,c2_virtual_secs + 9 -c2_virtt.it_value.tv_sec,999999-c2_virtt.it_value.tv_usec/1000);
 			getitimer(ITIMER_PROF,&c2_virtt);
-			printf("Child2 fib = %d \n Child2 ProfTime=%ld sec : %ld msec\n",fib,c2_prof_secs + 9 -c2_proft.it_value.tv_sec,999999-c2_proft.it_value.tv_usec/1000);
+			printf("Child2 ProfTime=%ld sec : %ld msec\n",fib,c2_prof_secs + 9 -c2_proft.it_value.tv_sec,999999-c2_proft.it_value.tv_usec/1000);
 			
 			
 		}else{
-			fib = fibonacci(atoi(argv[3));
+			fib = fibonacci(atoi(argv[3]));
 			// 打印子进程 1所花费的 3种时间值
 			getitimer(ITIMER_REAL,&c1_realt);
-			printf("Parent fib = %d \n Parent RealTime=%ld sec : %ld msec\n",fib,p_real_secs + 9 -p_realt.it_value.tv_sec,999999-p_realt.it_value.tv_usec/1000);
 			getitimer(ITIMER_VIRTUAL,&c1_virtt);
-			printf("Parent fib = %d \n Parent VirtualTime=%ld sec : %ld msec\n",fib,p_virtual_secs + 9 -p_virtt.it_value.tv_sec,999999-p_virtt.it_value.tv_usec/1000);
 			getitimer(ITIMER_PROF,&c1_virtt);
-			printf("Parent fib = %d \n Parent ProfTime=%ld sec : %ld msec\n",fib,p_prof_secs + 9 -p_proft.it_value.tv_sec,999999-p_proft.it_value.tv_usec/1000);
-			
+			printf("Parent fib = %d \nParent RealTime=%ld sec : %ld msec\n",fib,p_real_secs + 9 -p_realt.it_value.tv_sec,999999-p_realt.it_value.tv_usec/1000);
+			printf("Parent VirtualTime=%ld sec : %ld msec\n",fib,p_virtual_secs + 9 -p_virtt.it_value.tv_sec,999999-p_virtt.it_value.tv_usec/1000);		
+			printf("Parent ProfTime=%ld sec : %ld msec\n",fib,p_prof_secs + 9 -p_proft.it_value.tv_sec,999999-p_proft.it_value.tv_usec/1000);
 			waitpid(0, &status, 0);
 			waitpid(0, &status, 0);
 		}
@@ -172,10 +172,10 @@ static void psig_prof(void){
 static void c1sig_real(void){
 	c1_real_secs+=10;
 }
-static void c1ig_virtual(void){
+static void c1sig_virtual(void){
 	c1_virtual_secs+=10;
 }
-static void c1ig_prof(void){
+static void c1sig_prof(void){
 	c1_prof_secs+=10;
 }
 
